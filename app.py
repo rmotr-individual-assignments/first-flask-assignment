ee327c1154d7e0496dab453b2e6dcf2435f2fd19
Marianna Mangat <mangat.marianna@gmail.com>
2017-05-15 19:55:06 -0700

    2 requirements

--- app.py
+++ app.py
@@ -6,3 +6,10 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return "Hello World!"
+
+@app.route('/python')
+def python():
+    html = """<html><h1>Python Programming Language</h1></html>"""
+    return html
+
+
