1b37f9fe69fba09059697f3662aa9f2e3bde7a2f
dave-ford <ddford@gmail.com>
2017-06-07 21:37:10 -0400

    Create app.py

--- app.py
+++ app.py
@@ -5,4 +5,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    pass
+    return "Hello World"
+
+@app.route('/python')
+def py_route():
+    html = "<html><hl>Python Programming Language</hl></html>"
+    return html
