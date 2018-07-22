421a0dca4218727f7cfb8ba662cf9ebb6a0c08da
Craig Rupp <craigrupp@utexas.edu>
2018-07-22 20:01:57 +0000

    first-flask basic route returns

--- app.py
+++ app.py
@@ -5,4 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    pass
+    return 'Hello World'
+
+@app.route('/python')
+def python_response():
+    return """
+        <html>
+            <h1>Python Programming Language</h1>
+        </html>
+    """
