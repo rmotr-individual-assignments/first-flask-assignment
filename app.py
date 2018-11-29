e7c1c3be4b2ecff1c8ac957c66623f805416e971
Kristoffer Andersson <kristoffer.andersson@gu.se>
2018-11-29 08:24:26 +0100

    solve Requirement 2

--- app.py
+++ app.py
@@ -6,3 +6,8 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return "Hello World"
+
+
+@app.route('/python')
+def python():
+    return "<html><h1>Python Programming Language</h1></html>"
